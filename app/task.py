63af9080794a8cb1f83800c40bdc48b87bb7f947
Ethan Feldman <ejfeldman7@gmail.com>
2024-04-01 22:36:12 -0700

    Update task.py

--- app/task.py
+++ app/task.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 try:
     # sys.path.insert(0, 'ejfeldman7/which_parent')  # noqa
-    from ..library.model import ResNetWrapper # noqa
+    from library.model import ResNetWrapper # noqa
 except ModuleNotFoundError:
     sys.path.insert(0, '/Users/ejfel/Documents/Github/which_parent')  # noqa
     from library.model import ResNetWrapper # noqa
