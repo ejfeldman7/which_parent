21bf2af93e0081347c86c99928600c7f9f23e571
Ethan Feldman <ejfeldman7@gmail.com>
2024-04-01 22:21:40 -0700

    Adding docstrings and type painting

--- library/model.py
+++ library/model.py
@@ -9,24 +9,35 @@ from utils.image_loader import convert_to_png
 
 
 class ResNetWrapper(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes: int):
         super(ResNetWrapper, self).__init__()
         self.resnet = models.resnet50(pretrained=True)
         self.fc = nn.Linear(2048, num_classes)
 
-    def set_features(self, file1, file2, child_img):
+    def set_features(self, file1: str, file2: str, child_img: str):
+        '''
+        Extracts features from the images for parents and child, 
+        and sets them as respective attributes
+        '''
         self.parent1 = self.extract_features(file1)
         self.parent2 = self.extract_features(file2)
         self.child = self.extract_features(child_img)
 
-    def extract_features(self, img_path):
+    def extract_features(self, img_path: str) -> torch.Tensor:
+        '''
+        Extracts features from an image and returns a tensor
+        '''
         img = convert_to_png(img_path)
         img_tensor = F.to_tensor(img)
         img_tensor = torch.unsqueeze(img_tensor, 0)
         features = self.resnet(img_tensor[:, :3, :, :])
         return features
 
-    def get_similarities(self):
+    def get_similarities(self) -> str:
+        '''
+        Computes the cosine similar of the child image with the parent images.
+        Returns which parent has the greatest similarity to the child.
+        '''
         similarity1 = torch.cosine_similarity(self.child, self.parent1, dim=1)
         similarity2 = torch.cosine_similarity(self.child, self.parent2, dim=1)
 
--- utils/image_loader.py
+++ utils/image_loader.py
@@ -1,9 +1,12 @@
 from PIL import Image
 
-def convert_to_png(file_path):
+def convert_to_png(file_path: str) -> Image.Image:
+    '''
+    Convert file path to PNG format
+    '''
     try:
         image = Image.open(file_path).convert("RGB")
-        if image.format != 'PNG':
+        if image.format not in ['JPG', 'JPEG', 'PNG']:
             image = image.convert('RGBA')
             image.save('output.png', format='PNG')
         return image
