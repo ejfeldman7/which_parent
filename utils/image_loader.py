6d3e5f68fdaacf51d988dec9942a9c9f2e127e3c
Ethan Feldman <ejfeldman7@gmail.com>
2024-04-02 00:11:03 -0700

    Update image_loader.py

--- utils/image_loader.py
+++ utils/image_loader.py
@@ -1,15 +1,15 @@
-from PIL import Image
+# from PIL import Image
 
 
-def convert_to_png(file_path: str)e:
-    '''
-    Convert file path to PNG format
-    '''
-    try:
-        image = Image.open(file_path).convert("RGB")
-        if image.format not in ['JPG', 'JPEG', 'PNG']:
-            image = image.convert('RGBA')
-            # image.save('output.png', format='PNG')
-        return image
-    except IOError:
-        print("Unable to open or convert the image.")
+# def convert_to_png(file_path: str)e:
+#     '''
+#     Convert file path to PNG format
+#     '''
+#     try:
+#         image = Image.open(file_path).convert("RGB")
+#         if image.format not in ['JPG', 'JPEG', 'PNG']:
+#             image = image.convert('RGBA')
+#             # image.save('output.png', format='PNG')
+#         return image
+#     except IOError:
+#         print("Unable to open or convert the image.")
