4328f73026b907199ddfc9c4bf66ef0cb40dc7a5
Ethan Feldman <ejfeldman7@gmail.com>
2024-04-01 22:57:45 -0700

    Update model.py

--- library/model.py
+++ library/model.py
@@ -6,10 +6,10 @@ from torchvision.transforms import functional as F
 
 try:
     # sys.path.insert(0, '/ejfeldman7/which_parent')  # noqa
-    from utils.image_loader import ResNetWrapper # noqa
+    from utils.image_loader import convert_to_png # noqa
 except ModuleNotFoundError:
     sys.path.insert(0, '/Users/ejfel/Documents/Github/which_parent')  # noqa
-    from utils.image_loader import ResNetWrapper # noqa
+    from utils.image_loader import convert_to_png # noqa
 
 
 class ResNetWrapper(nn.Module):
